7c3b1f1ef9ebcb4d6bd80638bd20775596e20a08
subbugct <subbu.london@gmail.com>
2017-09-13 23:42:23 +0100

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 // @Component is a decorator  
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // it refers in index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
